2a83839fdfdfa2c5ce63d28b527279a0b01562d1
quewon <36914642+quewon@users.noreply.github.com>
2026-07-13 14:28:58 +0200

    better tokenization

--- minishell.c
+++ minishell.c
@@ -12,12 +12,16 @@
 
 #include "minishell.h"
 
-void	init_token(t_token *token, int datasize)
+t_token	*new_token(int datasize)
 {
+	t_token *token;
+
+	token = malloc(sizeof(t_token));
 	token->data = malloc(datasize + 1);
 	token->data[0] = 0;
 	token->type = 0;
 	token->next = NULL;
+	return (token);
 }
 
 void	print_tokens(t_token *token)
@@ -29,77 +33,113 @@ void	print_tokens(t_token *token)
 	}
 }
 
-t_token	*split_tokens(char *buffer, int size)
+t_token	*split_tokens(char *buffer)
 {
-	int	i;
-	int	j;
-	char	c;
 	t_token	*token;
 	t_token	*list;
+	int	i;
 	int	state;
 
-	token = malloc(sizeof(t_token));
-	init_token(token, size);
-	list = token;
+	token = new_token(ft_strlen(buffer));
 	i = 0;
-	j = 0;
-	state = 0; // 0: normal mode, 1: quoted mode
-	while (buffer[i] != '\0')
-	{
-		c = buffer[i];
 
-		if (c == '\"' || c == '\'')
+	list = token;
+	state = 0; // 0: default, 1: single quote, 2: double quote
+	while (*buffer != '\0')
+	{
+		if (state == 0)
 		{
-			if (c == '\"')
-				token->type = 1;
-			else
-				token->type = 2;
-			if (state == 0)
+			if (*buffer == '\'')
 			{
 				state = 1;
+				token->data[i++] = '\'';
+			}
+			else if (*buffer == '\"')
+			{
+				state = 2;
+				token->data[i++] = '\"';
+			}
+			else if (*buffer == '\\')
+			{
+				token->data[i++] = *(++buffer);
+			}
+			else if (*buffer == ' ')
+			{
+				if (i > 0)
+				{
+					token->data[i] = '\0';
+					token->next = new_token(ft_strlen(buffer));
+					token = token->next;
+					i = 0;
+				}
+			}
+			else if (*buffer == ';' || *buffer == '>' || *buffer == '<' || *buffer == '&' || *buffer == '|')
+			{
+				if (i > 0)
+				{
+					token->data[i] = '\0';
+					token->next = new_token(ft_strlen(buffer));
+					token = token->next;
+					i = 0;
+				}
+
+				token->data[0] = *buffer;
+				token->data[1] = 0;
+				token->type = *buffer;
+				token->next = new_token(ft_strlen(buffer));
+				token = token->next;
 			}
 			else
+			{
+				token->data[i++] = *buffer;
+			}
+		}
+		else if (state == 1)
+		{
+			token->data[i++] = *buffer;
+			if (*buffer == '\'')
 			{
 				state = 0;
 			}
 		}
-		else if (state == 0 && c == ' ')
+		else if (state == 2)
 		{
-			token->data[j] = 0;
-			j = 0;
-			token->next = malloc(sizeof(t_token));
-			token = token->next;
-			init_token(token, size);
+			token->data[i++] = *buffer;
+			if (*buffer == '\"')
+			{
+				state = 0;
+			}
 		}
-		else
-		{
-			token->data[j++] = c;
-		}
-
-		i++;
+		buffer++;
+	}
+	if (i > 0)
+	{
+		token->data[i] = 0;
+		i = 0;
 	}
 	return (list);
 }
 
-void	parse_tokens(t_token *token)
-{
-	char	*var;
-	char	*value;
-	char	*beforevar;
+// void	parse_tokens(t_token *token)
+// {
+// 	char	*var;
+// 	char	*value;
+// 	char	*beforevar;
 
-	while (token)
-	{
-		if (token->type <= 1)
-		{
-			var = ft_strchr(token->data, '$');
-			if (var != NULL)
-				value = getenv(var + 1);
-			else
-				value = "";
-		}
-		token = token->next;
-	}
-}
+// 	while (token)
+// 	{
+// 		if (token->type <= 1)
+// 		{
+// 			var = ft_strchr(token->data, '$');
+// 			if (var != NULL)
+// 				value = getenv(var + 1);
+// 			else
+// 				value = "";
+// 			printf("%s", value);
+// 		}
+// 		token = token->next;
+// 	}
+// }
 
 void run(t_token *token)
 {
@@ -129,7 +169,8 @@ int	main(void)
 	t_token	*tokens;
 
 	buffer = readline("$ ");
-	tokens = split_tokens(buffer, 1000);
-	parse_tokens(tokens);
+	tokens = split_tokens(buffer);
+	print_tokens(tokens);
+	// parse_tokens(tokens);
 	// run(tokens);
 }
